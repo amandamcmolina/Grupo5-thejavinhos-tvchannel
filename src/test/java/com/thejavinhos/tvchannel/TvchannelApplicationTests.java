f1da9e81151406a47f1ca3cb39660d06af393ad0
amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>
2020-10-27 19:32:35 -0300

    up project

---
+++ src/test/java/com/thejavinhos/tvchannel/TvchannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejavinhos.tvchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvchannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
