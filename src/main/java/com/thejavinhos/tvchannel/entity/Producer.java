690a90234d693605360585d85055115f8bb7d925
Renato Avellar <54222065+DonRenato@users.noreply.github.com>
2020-10-28 20:41:04 -0300

    add: lombok (#5)

--- src/main/java/com/thejavinhos/tvchannel/entity/Producer.java
+++ src/main/java/com/thejavinhos/tvchannel/entity/Producer.java
@@ -1,9 +1,13 @@
 package com.thejavinhos.tvchannel.entity;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Producer {
     @Id
@@ -21,43 +25,5 @@ public class Producer {
 
 
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-    public void setPassword(int password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
