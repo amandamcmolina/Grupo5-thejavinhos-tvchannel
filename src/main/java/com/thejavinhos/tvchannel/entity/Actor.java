4e412f5a453bacc3e65a23f3b184ccced2bd2578
Renato Avellar <54222065+DonRenato@users.noreply.github.com>
2020-10-28 17:59:09 -0300

    feature: reserveActor (#4)

--- src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
+++ src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
@@ -1,6 +1,10 @@
 package com.thejavinhos.tvchannel.entity;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Actor {
@@ -27,6 +31,14 @@ public class Actor {
 
     @Column(name = "genre_work")
     private String genreWork;
+
+    @ElementCollection
+    @Column(name = "reservations")
+    private List<Date> reservations = new ArrayList();
+
+    private Date dateReserveBegin;
+    private Date dateReserveEnd;
+
     private int reservationTotal = 0;
 
 
@@ -100,5 +112,41 @@ public class Actor {
 
     public void setReservationTotal(int reservationTotal) {
         this.reservationTotal = reservationTotal;
+    }
+
+    public List<Date> datas(Date dataInicial, Date dataFinal){
+        List<Date> datas = new ArrayList<Date>();
+        datas.add(dataInicial);
+        Calendar inicio = Calendar.getInstance();
+        inicio.setTime(dataInicial);
+        while(inicio.getTime().before(dataFinal)){
+            inicio.add(Calendar.DAY_OF_YEAR,1);
+            datas.add(inicio.getTime());
+        }
+        return datas;
+    }
+
+    public List<Date> getReservations() {
+        return reservations;
+    }
+
+    public void setReservations(List<Date> reservations) {
+        this.reservations = reservations;
+    }
+
+    public Date getDateReserveBegin() {
+        return dateReserveBegin;
+    }
+
+    public void setDateReserveBegin(Date dateReserveBegin) {
+        this.dateReserveBegin = dateReserveBegin;
+    }
+
+    public Date getDateReserveEnd() {
+        return dateReserveEnd;
+    }
+
+    public void setDateReserveEnd(Date dateReserveEnd) {
+        this.dateReserveEnd = dateReserveEnd;
     }
 }
--- src/main/java/com/thejavinhos/tvchannel/service/ActorService.java
+++ src/main/java/com/thejavinhos/tvchannel/service/ActorService.java
@@ -5,6 +5,8 @@ import com.thejavinhos.tvchannel.repository.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class ActorService {
 
@@ -13,7 +15,23 @@ public class ActorService {
 
     public Actor saveActor(Actor actor) {
             return actorRepository.save(actor);
+    }
 
+    public Actor reservationDates(Actor actor, int id) {
+        var inicial = actor.getDateReserveBegin();
+        var end = actor.getDateReserveEnd();
+        var dates =  actor.datas(inicial,end);
+        return (Actor) actorRepository.findById(id).map(a ->{
+            if(a.getReservations().contains(inicial) || a.getReservations().contains(end)){
+                return "Indisponivel";
+            }
+            a.setReservations(dates);
+            return actorRepository.save(a);
 
+        }).orElseThrow();
+    }
+
+    public List<Actor> listAllActors() {
+        return (List<Actor>) actorRepository.findAll();
     }
 }
