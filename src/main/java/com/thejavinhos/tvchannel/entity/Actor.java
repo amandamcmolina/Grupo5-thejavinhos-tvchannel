107e61244cb6b11d0bc5ea945f13f1b5ded69146
Renato Avellar <54222065+DonRenato@users.noreply.github.com>
2020-10-28 20:57:06 -0300

    Reserve actor (#6)
    * feature: reserveActor
    
    * add: lombok (#5)
    
    Co-authored-by: amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>

--- src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
+++ src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
@@ -6,6 +6,12 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 @Entity
 public class Actor {
 
@@ -32,6 +38,7 @@ public class Actor {
     @Column(name = "genre_work")
     private String genreWork;
 
+
     @ElementCollection
     @Column(name = "reservations")
     private List<Date> reservations = new ArrayList();
@@ -42,77 +49,7 @@ public class Actor {
     private int reservationTotal = 0;
 
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-    public void setPassword(int password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public double getPayment() {
-        return payment;
-    }
-
-    public void setPayment(double payment) {
-        this.payment = payment;
-    }
-
-    public boolean isAvailable() {
-        return available;
-    }
-
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
-
-    public String getGenreWork() {
-        return genreWork;
-    }
-
-    public void setGenreWork(String genreWork) {
-        this.genreWork = genreWork;
-    }
-
-    public int getReservationTotal() {
-        return reservationTotal;
-    }
-
-    public void setReservationTotal(int reservationTotal) {
-        this.reservationTotal = reservationTotal;
-    }
 
     public List<Date> datas(Date dataInicial, Date dataFinal){
         List<Date> datas = new ArrayList<Date>();
@@ -126,27 +63,6 @@ public class Actor {
         return datas;
     }
 
-    public List<Date> getReservations() {
-        return reservations;
-    }
 
-    public void setReservations(List<Date> reservations) {
-        this.reservations = reservations;
-    }
 
-    public Date getDateReserveBegin() {
-        return dateReserveBegin;
-    }
-
-    public void setDateReserveBegin(Date dateReserveBegin) {
-        this.dateReserveBegin = dateReserveBegin;
-    }
-
-    public Date getDateReserveEnd() {
-        return dateReserveEnd;
-    }
-
-    public void setDateReserveEnd(Date dateReserveEnd) {
-        this.dateReserveEnd = dateReserveEnd;
-    }
 }
