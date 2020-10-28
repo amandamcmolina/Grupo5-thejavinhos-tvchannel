0810067b03462460876438e6ebd8d520a7fea686
DonRenato <ravellar.machado@gmail.com>
2020-10-28 17:37:56 -0300

    feature: reserveActor

--- src/main/java/com/thejavinhos/tvchannel/controller/ActorController.java
+++ src/main/java/com/thejavinhos/tvchannel/controller/ActorController.java
@@ -5,9 +5,9 @@ import com.thejavinhos.tvchannel.service.ActorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/actors")
@@ -20,5 +20,15 @@ public class ActorController {
     @PostMapping
     public ResponseEntity<Actor> createActor(@RequestBody Actor actor){
         return ResponseEntity.ok(actorService.saveActor(actor));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Actor>> listAll(){
+        return ResponseEntity.ok(actorService.listAllActors());
+    }
+
+    @PutMapping("/reserveActor/{id}")
+    public ResponseEntity<Actor> reservationActor(@PathVariable int id, @RequestBody Actor actor){
+        return ResponseEntity.ok(actorService.reservationDates(actor, id));
     }
 }
