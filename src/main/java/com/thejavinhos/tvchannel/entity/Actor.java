bc4a8e4a19bb53377c2afd70f576e33ed5ec35f9
DonRenato <ravellar.machado@gmail.com>
2020-10-27 22:03:21 -0300

    add: actor and producer

---
+++ src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
@@ -0,0 +1,104 @@
+package com.thejavinhos.tvchannel.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "gender")
+    private String gender;
+    @Column(name = "password")
+    private int password;
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "payment")
+    private double payment;
+
+    @Column(name = "available")
+    private boolean available;
+
+    @Column(name = "genre_work")
+    private String genreWork;
+    private int reservationTotal = 0;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public double getPayment() {
+        return payment;
+    }
+
+    public void setPayment(double payment) {
+        this.payment = payment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getGenreWork() {
+        return genreWork;
+    }
+
+    public void setGenreWork(String genreWork) {
+        this.genreWork = genreWork;
+    }
+
+    public int getReservationTotal() {
+        return reservationTotal;
+    }
+
+    public void setReservationTotal(int reservationTotal) {
+        this.reservationTotal = reservationTotal;
+    }
+}
---
+++ src/main/java/com/thejavinhos/tvchannel/service/ProducerService.java
@@ -0,0 +1,20 @@
+package com.thejavinhos.tvchannel.service;
+
+import com.thejavinhos.tvchannel.entity.Actor;
+import com.thejavinhos.tvchannel.entity.Producer;
+import com.thejavinhos.tvchannel.repository.ActorRepository;
+import com.thejavinhos.tvchannel.repository.ProducerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProducerService {
+    @Autowired
+    private ProducerRepository producerRepository;
+
+    public Producer saveProducer(Producer producer) {
+        return producerRepository.save(producer);
+
+
+    }
+}
