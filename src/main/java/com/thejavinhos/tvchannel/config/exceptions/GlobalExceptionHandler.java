1e65cd0ca4954f7673e5c74bdb9b3e8746380a09
DonRenato <ravellar.machado@gmail.com>
2020-10-28 22:26:33 -0300

    add: exception handler - update-reserves

---
+++ src/main/java/com/thejavinhos/tvchannel/config/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.thejavinhos.tvchannel.config.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    protected ResponseEntity<Object> handleConflicts(RuntimeException exception, WebRequest webRequest){
+        Map<String, String> message = Map.of("message",exception.getMessage());
+        return handleExceptionInternal(exception,message,new HttpHeaders(), HttpStatus.CONFLICT,webRequest);
+    }
+}
