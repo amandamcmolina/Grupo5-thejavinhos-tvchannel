f29b7080004fea8027eea6629e9dc3ab542fd5ce
amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>
2020-11-03 19:19:57 -0300

    add: contador de reservas na classe Actor

--- src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
+++ src/main/java/com/thejavinhos/tvchannel/entity/Actor.java
@@ -5,65 +5,73 @@ import javax.persistence.*;
 //@Data
 //@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Actor extends User{
+public class Actor extends User {
 
 
-    @Column(name = "gender")
-    private String gender;
+  @Column(name = "gender")
+  private String gender;
 
-    @Column(name = "payment")
-    private double payment;
+  @Column(name = "payment")
+  private double payment;
 
-    @Column(name = "available")
-    private boolean available;
+  @Column(name = "available")
+  private boolean available;
 
-    @Column(name = "genre_work")
-    private String genreWork;
+  @Column(name = "genre_work")
+  private String genreWork;
 
-    @Column(name = "is_admin")
-    private boolean isAdmin = false;
-
-
+  @Column(name = "is_admin")
+  private boolean isAdmin = false;
 
 
-    public String getGender() {
-        return gender;
-    }
+  private int contador;
 
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
+  public int getContador() {
+    return contador;
+  }
+
+  public void setContador(int contador) {
+    this.contador = contador;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
 
 
-    public double getPayment() {
-        return payment;
-    }
+  public double getPayment() {
+    return payment;
+  }
 
-    public void setPayment(double payment) {
-        this.payment = payment;
-    }
+  public void setPayment(double payment) {
+    this.payment = payment;
+  }
 
-    public boolean isAvailable() {
-        return available;
-    }
+  public boolean isAvailable() {
+    return available;
+  }
 
-    public void setAvailable(boolean available) {
-        this.available = available;
-    }
+  public void setAvailable(boolean available) {
+    this.available = available;
+  }
 
-    public String getGenreWork() {
-        return genreWork;
-    }
+  public String getGenreWork() {
+    return genreWork;
+  }
 
-    public void setGenreWork(String genreWork) {
-        this.genreWork = genreWork;
-    }
+  public void setGenreWork(String genreWork) {
+    this.genreWork = genreWork;
+  }
 
-    public boolean isIsAdmin() {
-        return isAdmin;
-    }
+  public boolean isIsAdmin() {
+    return isAdmin;
+  }
 
-    public void setIs(boolean isAdmin) {
-        this.isAdmin = isAdmin;
-    }
+  public void setIs(boolean isAdmin) {
+    this.isAdmin = isAdmin;
+  }
 }
