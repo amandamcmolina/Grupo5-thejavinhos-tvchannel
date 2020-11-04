2c341a12b687a73cbaacf542c752ec5f91496dfb
amandamcmolina <56768735+amandamcmolina@users.noreply.github.com>
2020-11-04 20:42:15 -0300

    add: campo para token

--- src/main/java/com/thejavinhos/tvchannel/swagger/SwaggerConfig.java
+++ src/main/java/com/thejavinhos/tvchannel/swagger/SwaggerConfig.java
@@ -2,12 +2,15 @@ package com.thejavinhos.tvchannel.swagger;
 
 import com.thejavinhos.tvchannel.entity.User;
 import io.swagger.models.Swagger;
+import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -28,7 +31,16 @@ public class SwaggerConfig {
         .apis(RequestHandlerSelectors.basePackage("com.thejavinhos.tvchannel"))
         .paths(PathSelectors.ant("/**"))
         .build()
-        .apiInfo(metaData());
+        .apiInfo(metaData())
+        .globalOperationParameters(Arrays.asList(
+            new ParameterBuilder()
+            .name("Authorization")
+            .description("Header para token JWT")
+            .modelRef(new ModelRef("string"))
+            .parameterType("header")
+            .required(false)
+            .build()
+        ));
 //        .ignoredParameterTypes(User.class)
 
 
